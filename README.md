823602d5c4e8926f236847f571654b3ade0533d0
Amruta Akut <amruta.akut@gmail.com>
2015-03-28 17:34:05 -0400

    Added Responsive behaavior to Learn page

--- README.md
+++ README.md
@@ -3,13 +3,11 @@ This web application helps individuals appearing for GRE exam in preparing their
 
 This application has 2 sections
 1. Learn
-	In this section the application presents a list of high frequency words displayed in a tabular format
-2. Test
+	In this section the application presents a list of high frequency words displayed in a tabular format. The wordlist is contains a set of high frequency words and it is stored in firebase
+2. Flash Cards
 	In this section, users can test their vocabulary skills by using the flash cards presented to them. The words for the flash cards are selected randomly which enables users to truly test their vocabulary skills
-3.	Wordlist 
-	The wordlist is contains a set of high frequency words and it is stored in firebase
 
-In addition to this, the app can also work offline. It is responsive, uses the latest service workers based on ES6 promises and caches the wordlist in IndexedDb to deliver a truly offline experience. 
+In addition to this, the app is designed to be mobile-first and can also work offline. It is responsive, uses the latest service workers based on ES6 promises and caches the wordlist in IndexedDb to deliver a truly offline experience. 
 
 To test it, visit https://buildyourvocabulary.appspot.com/ . 
 
@@ -22,12 +20,14 @@ To test it, visit https://buildyourvocabulary.appspot.com/ .
 - Gulp for build process and miscellaneous tasks
 
 #To-Do
-- Include the service worker polyfill
-- Include basic instructions in the Test page
-- Make the app mobile-compatible
+- Add test section
 - Incorporate routing
-- Improve UX
 - Incorporate testing (using chai, mocha, karma etc.)
+- Include the service worker polyfill
+
+#Known Issues
+- Learn page not working on IE
+- Not working at all on Chrome for iOS
 
 #Stretch Goals
 - Incorporate push notifications
--- vocabulary-builder/vocabulary-builder.js
+++ vocabulary-builder/vocabulary-builder.js
@@ -1,124 +1,143 @@
-Polymer('vocabulary-builder', {
+(function(window) {
 
-	wordlist : [],
+	var LEARN_PAGE_HEADING = "Build Your Vocabulary !",
+		MEMORIZE_PAGE_HEADING = "Memorize the word and click the flash card to show its meaning";
 
-	indices : [],
+	Polymer('vocabulary-builder', {
 
-	store : null,
-	db : null,
+		wordlist : [],
 
-	ready: function() {
-		this.selected = 0;
-	},
+		indices : [],
 
-	onError : function () {
-		this.$.wordlistRequest.go();
-	},
+		store : null,
+		db : null,
+		text : LEARN_PAGE_HEADING,
 
-	onDataRead : function () {
+		ready: function() {
+			this.selected = 0;
+		},
 
-		if(this.wordlist == null || this.wordlist == undefined) {
+		onError : function () {
 			this.$.wordlistRequest.go();
-			console.log('requesting wordlist to server');
-		}
-		else
-		{
-			this.generateArrayIndices();
-		}
-	},
+		},
 
-	onDbInitialized : function (event) {
-		// check if wordlist is there in the store. If yes, set wordlist 
+		onDataRead : function () {
 
-		event.preventDefault();
-		event.stopPropagation();
+			if(this.wordlist == null || this.wordlist == undefined) {
+				this.$.wordlistRequest.go();
+				console.log('requesting wordlist to server');
+			}
+			else
+			{
+				this.generateArrayIndices();
+			}
+		},
 
-		if(this.db != null) {
-			var ob = this.$.store.getData("json", "readonly");			
-		}
-		else
-			console.log('db instance is invalid');
-	},
+		onDbInitialized : function (event) {
+			// check if wordlist is there in the store. If yes, set wordlist 
 
-	wordList: [],
-
-	toggleDrawer: function () {
-		this.$.coreDrawerPanel.togglePanel();
-	},
-
-	flip: function(e) {
-		e.currentTarget.parentElement.toggle();	
-	},
-	
-	generateSubWordList : function() {
-
-		var list = [];
-
-		for(var i = 0; i < this.indices.length ; i++) {
-			list[i] = this.wordlist[this.indices[i]];
-		}
-
-		this.sublist = list;
-
-	},
-
-	generateArrayIndices : function () {
-
-		if(this.wordlist != null && this.wordlist != undefined && this.wordlist.length > 0) {
-
-			console.log('fetched wordlist');
-			this.$.randomNumberGenerator.generateRandomNumbers();
-			this.$.pager.data = this.wordlist;
+			event.preventDefault();
+			event.stopPropagation();
 
 			if(this.db != null) {
-
-			    console.log('storing data');
-			    this.$.store.storeData("json", "readwrite");
+				var ob = this.$.store.getData("json", "readonly");			
 			}
-		}			
-	},
+			else
+				console.log('db instance is invalid');
+		},
 
-	indicesChanged : function () {
-		if(this.wordlist != null && this.wordlist.length > 0 && this.indices.length > 0)
-			this.generateSubWordList();
-		else if(this.indices.length == 0)
-			this.$.randomNumberGenerator.generateRandomNumbers();
-	},
+		wordList: [],
 
-	onPaginationComplete : function (e) {
-		this.results = e.detail.data;	
-		this.$.vocabTable.refresh();
-		this.updateCurrentRange();	
-	},
+		toggleDrawer: function () {
+			this.$.coreDrawerPanel.togglePanel();
+		},
 
-	onPageChange : function (e) {
-		this.results = e.detail.data;
-		this.$.vocabTable.refresh();	
-		this.updateCurrentRange();	
-	},
+		flip: function(e) {
+			e.currentTarget.parentElement.toggle();	
+		},
+		
+		generateSubWordList : function() {
 
-	updateCurrentRange : function() {
-		var currentPage = this.$.pager.currentpage;
-		var pageCount = this.$.pager.pageCount;
-		var addPages, pages = [];
+			var list = [];
 
-		addPages = function (fromPage, toPage) {
-
-			for(var i=0 ; i<= (toPage - fromPage) ; i++) {
-				pages[i] = fromPage + i;
+			for(var i = 0; i < this.indices.length ; i++) {
+				list[i] = this.wordlist[this.indices[i]];
 			}
-			
-		}
 
-		var pagesToDisplay = 10;
+			this.sublist = list;
 
-		if(currentPage >= 0 && currentPage <= 4) 
-			addPages (0, 9);
-		else if(currentPage >= pageCount - 4)
-			addPages (pageCount - 9, pageCount);
-		else
-			addPages (currentPage - 4, currentPage + 5);
-		this.$.pager.currentRange = pages;
-	}	
-})
+		},
 
+		generateArrayIndices : function () {
+
+			if(this.wordlist != null && this.wordlist != undefined && this.wordlist.length > 0) {
+
+				console.log('fetched wordlist');
+				this.$.randomNumberGenerator.generateRandomNumbers();
+				this.$.pager.data = this.wordlist;
+
+				if(this.db != null) {
+
+				    console.log('storing data');
+				    this.$.store.storeData("json", "readwrite");
+				}
+			}			
+		},
+
+		indicesChanged : function () {
+			if(this.wordlist != null && this.wordlist.length > 0 && this.indices.length > 0)
+				this.generateSubWordList();
+			else if(this.indices.length == 0)
+				this.$.randomNumberGenerator.generateRandomNumbers();
+		},
+
+		onPaginationComplete : function (e) {
+			this.results = e.detail.data;	
+			this.$.vocabTable.refresh();
+			this.updateCurrentRange();	
+		},
+
+		onPageChange : function (e) {
+			this.results = e.detail.data;
+			this.$.vocabTable.refresh();	
+			this.updateCurrentRange();	
+		},
+
+		updateCurrentRange : function() {
+			var currentPage = this.$.pager.currentpage;
+			var pageCount = this.$.pager.pageCount;
+			var addPages, pages = [];
+
+			addPages = function (fromPage, toPage) {
+
+				for(var i=0 ; i<= (toPage - fromPage) ; i++) {
+					pages[i] = fromPage + i;
+				}
+				
+			}
+
+			var pagesToDisplay = 10;
+
+			if(currentPage >= 0 && currentPage <= 4) 
+				addPages (0, 9);
+			else if(currentPage >= pageCount - 4)
+				addPages (pageCount - 9, pageCount);
+			else
+				addPages (currentPage - 4, currentPage + 5);
+			this.$.pager.currentRange = pages;
+		},
+
+		updateLearnHeading : function() {
+			this.text = LEARN_PAGE_HEADING;
+			this.$.mainToolbarText.className = "";
+			this.$.coreDrawerPanel.closeDrawer();
+		},
+
+		updateFlashCardHeading : function() {
+			this.text = MEMORIZE_PAGE_HEADING;
+			this.$.mainToolbarText.className = "instruction";
+			this.$.coreDrawerPanel.closeDrawer();
+		}	
+	})
+
+}(this));
